0936b0ed6e6c758969226c88e4991789c487ac81
JamnalHardik <rajputhardik12@gmail.com>
2021-05-31 19:39:07 +0530

    XYZ

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -5,13 +5,25 @@ const Signup = () => {
     const SignupForm = () => {
         return (
             <div>
-                <h2 className="text-center bg-success">Digital Health Card</h2>
-                <div className="mt-5 container">
-                    <h4 className="text-center">Signup Form</h4>
-                    <button className="btn btn-primary">Submit</button>
-                </div>
-
-
+                     <div id ="header-main">
+            <h1>Digital Health Card</h1>
+        </div>
+        <div classname="container">
+        <form>
+  <div classname="mb-3">
+    <label for="exampleInputEmail1" classname="form-label">Email address</label>
+    <input type="email" classname="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+  </div>
+  <div classname="mb-3">
+    <label for="exampleInputPassword1" classname="form-label">Password</label>
+    <input type="password" classname="form-control" id="exampleInputPassword1" />
+  </div>
+  <div classname="mb-3 form-check">
+    <input type="checkbox" classname="form-check-input" id="exampleCheck1" />
+    <label classname="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" classname="btn btn-primary">Submit</button>
+</form>
             </div>
         )
     }
