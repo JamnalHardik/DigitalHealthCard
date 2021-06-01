8626cd35c8fbfdf6896de9abf918a7646715f5ce
Poojan Pandya <pandyapoojan2000@gmail.com>
2021-06-01 23:56:13 +0530

    Add Submit functionality

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import '../styles.css'
 import { Form, FormGroup, Label, Input, FormText, Button, Col } from 'reactstrap';
-
 import { signup } from '../auth/helper/index'
 
 const Signup = () => {
   const [values, setValues] = useState({
-    firstname: "",
-    lastname: "",
+    firstName: "",
+    lastName: "",
     aadharNumber: "",
     email: "",
     password: "",
     dateOfBirth: "",
+    phoneNo: "",
     error: "",
     success: false,
     userSignup: "User Signup",
@@ -19,12 +19,53 @@ const Signup = () => {
   });
 
 
-  const { firstname, lastName, aadharNumber, email, password, dateOfBirth, error, success, userSignup } = values;
+  const { firstname, lastName, aadharNumber, email, password, dateOfBirth, error, success, userSignup, phoneNo } = values;
 
-  const handleChange = name => event => {
-    // ...values loads existing values
+  const handleSelect = name => event => {   
     setValues({ ...values, userSignup: event.target.value })
   }
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value })
+  }
+
+  // const handleChange
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false })
+    signup({ firstname, lastName, aadharNumber, email, password, dateOfBirth, phoneNo })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false })
+        } else {
+          setValues({
+            ...values,
+            firstname: "",
+            lastName: "",
+            aadharNumber: "",
+            email: "",
+            password: "",
+            dateOfBirth: "",
+            phoneNo: "",
+            error: "",
+            success: true
+          });
+        }
+      });
+  }
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">          
+          <div className="alert alert-danger" style={{ display: error ? "" : "none" }}> {error} </div>
+        </div>
+      </div>
+    )
+
+  }
+
 
   const SignupForm = () => {
     return (
@@ -36,41 +77,41 @@ const Signup = () => {
           <FormGroup row className="mb-2">
             <Label for="exampleSelect" sm={2}>Category</Label>
             <Col sm={10}>
-              <Input type="select" name="select" id="exampleSelect" onChange={handleChange()}>
+              <Input type="select" name="select" id="exampleSelect" onChange={handleSelect()}>
                 <option id="user">User Signup</option>
                 <option id="hospital">Hospital Signup</option>
               </Input>
             </Col>
-          </FormGroup>     
-            {(userSignup === "User Signup") && <div>
+          </FormGroup>
+          {(userSignup === "User Signup") && <div>
             <FormGroup row className="mb-2">
               <Label for="examplefirstname" sm={2}>First Name*</Label>
               <Col sm={10}>
-                <Input type="name" name="firstname" id="examplefirstname" placeholder="eg-Poojan" />
+                <Input type="name" name="firstname" id="examplefirstname" onChange={handleChange("firstName")} placeholder="eg-Poojan" />
               </Col>
             </FormGroup>
             <FormGroup row className="mb-2">
               <Label for="examplelastname" sm={2}>Last Name</Label>
               <Col sm={10}>
-                <Input type="name" name="lastname" id="examplelastname" placeholder="eg-Pandya" />
+                <Input type="name" name="lastname" id="examplelastname" placeholder="eg-Pandya" onChange={handleChange("lastName")} />
               </Col>
             </FormGroup>
             <FormGroup row className="mb-2">
               <Label for="exampleaadhar" sm={2}>Aadhar No*</Label>
               <Col sm={10}>
-                <Input type="name" name="aadhar" id="exampleaadhar" placeholder="12-Digit Aadhar Number" />
+                <Input type="name" name="aadhar" id="exampleaadhar" placeholder="12-Digit Aadhar Number" onChange={handleChange("aadharNumber")} />
               </Col>
             </FormGroup>
             <FormGroup row className="mb-2">
               <Label for="exampleEmail" sm={2}>Email</Label>
               <Col sm={10}>
-                <Input type="email" name="email" id="exampleEmail" placeholder="eg-poojanpandya@gmail.com" />
+                <Input type="email" name="email" id="exampleEmail" placeholder="eg-poojanpandya@gmail.com" onChange={handleChange("email")} />
               </Col>
             </FormGroup>
             <FormGroup row className="mb-2">
               <Label for="examplePassword" sm={2}>Password</Label>
               <Col sm={10}>
-                <Input type="password" name="password" id="examplePassword" placeholder="" />
+                <Input type="password" name="password" id="examplePassword" placeholder="" onChange={handleChange("password")} />
               </Col>
             </FormGroup>
             <FormGroup row className="mb-2">
@@ -82,18 +123,18 @@ const Signup = () => {
             <FormGroup row className="mb-2">
               <Label for="exampledob" sm={2}>Date of Birth*</Label>
               <Col sm={10}>
-                <Input type="date" name="dob" id="exampledob" placeholder="" />
+                <Input type="date" name="dob" id="exampledob" placeholder=""  onChange={handleChange("dateOfBirth")}/>
               </Col>
             </FormGroup>
             <FormGroup row className="mb-3">
               <Label for="examplePhone" sm={2}>Phone No</Label>
               <Col sm={10}>
-                <Input type="tel" name="phone" id="examplePhone" placeholder="" />
+                <Input type="tel" name="phone" id="examplePhone" placeholder="" onChange={handleChange("phoneNo")} />
               </Col>
             </FormGroup>
-            <div id="center"><Button color="primary" size="lg">Submit</Button>{' '}</div>
+            <div id="center"><Button onClick={onSubmit} color="primary" size="lg">Submit</Button>{' '}</div>
           </div>}
-          
+
           {userSignup === "Hospital Signup" && <div>
             <input type="button" value="xyz" />
           </div>}
@@ -104,8 +145,9 @@ const Signup = () => {
   }
 
   return (
-    <div>     
-        { SignupForm() }      
+    <div>
+      {errorMessage()} 
+      {SignupForm()}
     </div>
   )
 }
