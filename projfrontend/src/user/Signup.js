cb20279584a53d929081a350d17f0944aca401de
JamnalHardik <rajputhardik12@gmail.com>
2021-06-01 14:43:36 +0530

    XYZ

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles.css'
+import { Form, FormGroup, Label, Input, FormText, Button ,Col} from 'reactstrap';
+
 
 const Signup = () => {
   const SignupForm = () => {
@@ -8,27 +10,20 @@ const Signup = () => {
         <div id="header-main">
           <h1>Digital Health Card</h1>
         </div>
-        <div classname="container">
-          <form id="form-main">
-          <div classname="mb-3">
-              <label for="exampleInputEmail1" classname="form-label">First Name </label>
-              <input type="name" classname="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            </div>
-            <div classname="mb-3">
-              <label for="exampleInputEmail1" classname="form-label">Email address</label>
-              <input type="email" classname="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            </div>
-            <div classname="mb-3">
-              <label for="exampleInputPassword1" classname="form-label">Password</label>
-              <input type="password" classname="form-control" id="exampleInputPassword1" />
-            </div>
-            <div classname="mb-3 form-check">
-              <input type="checkbox" classname="form-check-input" id="exampleCheck1" />
-              <label classname="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" classname="btn btn-primary">Submit</button>
-          </form>
-        </div>
+        <Form id="form-main">
+      <FormGroup row className="mb-3">
+        <Label for="exampleEmail" sm={2}>Email</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={2}>Password</Label>
+        <Col sm={10}>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </Col>
+      </FormGroup>
+      </Form>
       </div>
     )
   }
