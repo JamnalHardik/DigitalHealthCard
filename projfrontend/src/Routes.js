ea572aa28f6b53da5b7f63c1e4b2f09980a3308a
Poojan Pandya <pandyapoojan2000@gmail.com>
2021-06-08 00:05:46 +0530

    xyz

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -6,6 +6,7 @@ import Signin from './user/Signin'
 import HealthCard from './facility/HealthCard'
 import UserRoute from './auth/helper/UserRoute'
 import HospitalRoute from './auth/helper/HospitalRoute'
+import HospitalUI from './facility/HospitalUI' 
 
 const Routes = () => {
     return (
@@ -13,7 +14,8 @@ const Routes = () => {
             <Switch>
                 <Route path="/signup" exact component={Signup} />
                 <Route path="/" exact component={Signin} />                
-                <UserRoute path="/user/healthcard" component={HealthCard} />
+                <UserRoute path="/user/healthcard" exact component={HealthCard} />
+                <HospitalRoute path="/hospital/dashboard" exact component={HospitalUI} />
             </Switch>
         </Router>
     )
