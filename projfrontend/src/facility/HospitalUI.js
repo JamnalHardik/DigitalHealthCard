5991cbd2266abf1251b019500e972b02ab2f2874
JamnalHardik <rajputhardik12@gmail.com>
2021-06-07 23:33:42 +0530

    XYZ

---
+++ projfrontend/src/facility/HospitalUI.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../core/Navbar_Hospital";
+const HospitalUI = () => {
+    return (
+        <div>
+            
+      <Navbar />
+        </div>
+    )
+}
+
+
+export default HospitalUI
