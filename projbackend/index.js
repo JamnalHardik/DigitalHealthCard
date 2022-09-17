1fd3fadbf78de396a111f6834c30229a52015241
Hardik <rajputhardik12@gmail.com>
2022-09-17 12:03:09 +0530

    Database configuration change for deployment

--- projbackend/index.js
+++ projbackend/index.js
@@ -1,26 +1,36 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const port = 8000;
-
+const port = process.env.PORT || 8000;
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
 const authRoutes = require("./routes/auth");
 const hospitalRoutes = require("./routes/hospital");
 const userRoutes = require("./routes/user");
 
-mongoose.connect('mongodb://localhost:27017/healthCard', {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log("Database Connected");
-});
-
+  });
 
 // Middlewares
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
@@ -30,7 +40,6 @@ app.use("/api", authRoutes);
 app.use("/api", hospitalRoutes);
 app.use("/api", userRoutes);
 
-
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
