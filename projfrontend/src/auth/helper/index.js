a5a6b48e4b882222eeea37d33b10db0f9bd50096
Hardik <rajputhardik12@gmail.com>
2022-09-17 13:34:07 +0530

    React Backend API

--- projfrontend/src/auth/helper/index.js
+++ projfrontend/src/auth/helper/index.js
@@ -1,80 +1,80 @@
 import { API } from "../../backend";
 
-export const signupUser = user => {    
-    return fetch("http://localhost:8000/api/user/signup", {
-        method: "POST",
+export const signupUser = (user) => {
+  return fetch(`${API}/user/signup`, {
+    method: "POST",
 
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },        
-        body: JSON.stringify(user)                
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })        
-        .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
-export const signupHospital = user => {    
-    return fetch("http://localhost:8000/api/hospital/signup", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },        
-        body: JSON.stringify(user)                
+export const signupHospital = (user) => {
+  return fetch(`${API}/hospital/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })        
-        .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
-export const signin = user => {
-    return fetch("http://localhost:8000/api/signin", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
+export const signin = (user) => {
+  return fetch(`${API}/api/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 export const authenticate = (data, next) => {
-    if (typeof window !== undefined) {
-        localStorage.setItem("jwt", JSON.stringify(data));
-        next();
-    }
-}
-
-export const signout = next => {
-    localStorage.removeItem("jwt");
-    localStorage.removeItem("userId");
-    localStorage.removeItem("lastName");
-    localStorage.removeItem("firstName");
-    localStorage.removeItem("phoneNo");
-    localStorage.removeItem("dateOfBirth");
+  if (typeof window !== undefined) {
+    localStorage.setItem("jwt", JSON.stringify(data));
     next();
-    return fetch(`${API}/signout`, {
-        method: "GET"
-    })
-        .then(response => console.log("Signout success"))
-        .catch(err => console.log(err));
-}
+  }
+};
+
+export const signout = (next) => {
+  localStorage.removeItem("jwt");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("lastName");
+  localStorage.removeItem("firstName");
+  localStorage.removeItem("phoneNo");
+  localStorage.removeItem("dateOfBirth");
+  next();
+  return fetch(`${API}/signout`, {
+    method: "GET",
+  })
+    .then((response) => console.log("Signout success"))
+    .catch((err) => console.log(err));
+};
 
 export const isAuthenticated = () => {
-    if (typeof window == "undefined") {
-        return false
-    }
-    if (localStorage.getItem("jwt")) {
-        return JSON.parse(localStorage.getItem("jwt"))
-    } else {
-        return false
-    }
-}
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
